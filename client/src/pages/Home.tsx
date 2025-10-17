cbb987a90451cc86eaf15601caa80af8b5a4dba9
emailsuporte55 <38824126-emailsuporte55@users.noreply.replit.com>
2025-10-17 19:43:05 +0000

    Restored to 'c1b9172a97adc54c8d6e8296282e559f202bc134'
    Replit-Restored-To: c1b9172a97adc54c8d6e8296282e559f202bc134

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import Header from '@/components/Header';
 import PageTitle from '@/components/PageTitle';
+import VideoSection from '@/components/VideoSection';
 import HeroSection from '@/components/HeroSection';
 import Carousel from '@/components/Carousel';
 import InfoSection from '@/components/InfoSection';
@@ -68,6 +69,7 @@ const Home = () => {
       <div className={showCepModal ? 'hidden' : 'block'}>
         <Header />
         <PageTitle />
+        <VideoSection />
         <HeroSection />
         <Carousel />
         <InfoSection />
