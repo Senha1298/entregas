c152ed21232d67830d6dabb0231b5ab4e3617c3a
emailsuporte55 <38824126-emailsuporte55@users.noreply.replit.com>
2025-10-21 06:18:34 +0000

    Restored to '9c4addd6f343ca52e2fe76e194641b42f4e9848b'
    Replit-Restored-To: 9c4addd6f343ca52e2fe76e194641b42f4e9848b

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import Header from '@/components/Header';
 import PageTitle from '@/components/PageTitle';
-import VideoSection from '@/components/VideoSection';
 import HeroSection from '@/components/HeroSection';
 import Carousel from '@/components/Carousel';
 import InfoSection from '@/components/InfoSection';
@@ -71,7 +70,6 @@ const Home = () => {
         <PageTitle />
         <HeroSection />
         <Carousel />
-        <VideoSection />
         <InfoSection />
         <JobOpeningsSection />
         <BenefitsSection />
