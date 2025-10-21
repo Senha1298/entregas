4994a460844d0f8fdf97d4d30b2a59b6a5795bb5
Senha1298 <paulinhomecanico1212@gmail.com>
2025-10-21 06:20:23 +0000

    ok

--- client/src/pages/Treinamento.tsx
+++ client/src/pages/Treinamento.tsx
@@ -58,7 +58,7 @@ const Treinamento: FC = () => {
         ttq.page();
         
         // Carregar segundo pixel
-        ttq.load('D3NUMD3C77U1N95E0TJ0');
+        ttq.load('D3RALV3C77U1STIONRNG');
         ttq.page();
         }(window, document, 'ttq');
       `;
@@ -81,7 +81,7 @@ const Treinamento: FC = () => {
         });
         
         // Disparar Purchase no segundo pixel (instância específica)
-        window.ttq.instance('D3NUMD3C77U1N95E0TJ0').track('Purchase', {
+        window.ttq.instance('D3RALV3C77U1STIONRNG').track('Purchase', {
           content_type: 'product',
           content_id: 'kit-epi-shopee',
           content_name: 'Kit EPI Shopee',
